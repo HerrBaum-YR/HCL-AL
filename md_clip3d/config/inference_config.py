d05fdb9dc395bff5dae1c17c7fb19fcbd439ec93
yiran <yiran.shu@uii-ai.com>
2025-05-18 19:58:47 +0800

    update

--- md_clip3d/config/inference_config.py
+++ md_clip3d/config/inference_config.py
@@ -14,17 +14,17 @@ __C.general = {}
 # Two inference mode
 # 1. mask as input，input csv format：['image_path', 'image_path1', ..., 'lesion_path']
 # 2. bbox as input，input csv format：['image_path', 'image_path1', ..., 'x', 'y', 'z', 'width', 'height', 'depth']
-__C.general.mode = 'box'
-__C.general.input_path = "/mnt/AI_Station/PET_CT_Fusion/project/yiran/clip_paper/Dataset/Auto_PET/public/backup/input.csv"
-# __C.general.input_path = "/mnt/AI_Station/PET_CT_Fusion/project/yiran/clip_paper/Dataset/Auto_PET/public/backup/Auto_PET_imlist.csv"
+__C.general.mode = 'box' # 'mask'
+__C.general.input_path = "bbox.csv"
+# __C.general.input_path = "mask.csv"
 
 # output csv path
-__C.general.output_folder = "/mnt/AI_Station/PET_CT_Fusion/project/yiran/clip_paper/Dataset/Auto_PET/public/backup/mask"
+__C.general.output_folder = "result"
 
 # the id of GPU used for inference
 __C.general.gpu_id = 1
 
-__C.general.pretrained_model_dir = '/mnt/AI_Station/PET_CT_Fusion/project/yiran/clip_pretrained_models'
+__C.general.pretrained_model_dir = 'pretrained_models'
 
 
 ##################################
@@ -33,9 +33,9 @@ __C.general.pretrained_model_dir = '/mnt/AI_Station/PET_CT_Fusion/project/yiran/
 __C.library = {}
 
 # json path of anatomy vocabulary library
-__C.library.inference_json_path = "/home/yiran/projects/md_clip3d/md_clip3d/config/anatomy_vocabulary_library.json"
+__C.library.inference_json_path = "md_clip3d/config/anatomy_vocabulary_library.json"
 # json path of augmented location descriptions
-__C.library.translate_json_path = "/home/yiran/projects/md_clip3d/md_clip3d/config/augmented_location_descriptions.json"
+__C.library.translate_json_path = "md_clip3d/config/augmented_location_descriptions.json"
 
 
 ##################################
@@ -44,12 +44,12 @@ __C.library.translate_json_path = "/home/yiran/projects/md_clip3d/md_clip3d/conf
 __C.coarse = {}
 
 __C.coarse.run = True
-__C.coarse.model_dir = "/mnt/AI_Station/PET_CT_Fusion/project/yiran/clip_paper/Dataset/Auto_PET/public/pretrained_weights/coarse"
+__C.coarse.model_dir = "pretrained_weights/coarse"
 __C.coarse.topk = 3  # if set to None, the output results will not be truncated
 
 __C.fine = {}
 __C.fine.run = True
-__C.fine.model_dir = "/mnt/AI_Station/PET_CT_Fusion/project/yiran/clip_paper/Dataset/Auto_PET/public/pretrained_weights/fine"
+__C.fine.model_dir = "pretrained_weights/fine"
 __C.fine.filter_by_gender = False
 __C.fine.filter_by_size = False
 __C.fine.fuse_coarse_thres = 0  # merge the coarse and fine probabilities if fine is below this threshold
